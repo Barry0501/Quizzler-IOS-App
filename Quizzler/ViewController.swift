b5498a258e1af7cf44ba7bf12f1e31cc74ed6b73
Tung Duong <laptung051@gmail.com>
2020-04-07 10:51:09 +0700

    Add some questions and update UI

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueBtn: UIButton!
+    @IBOutlet weak var falseBtn: UIButton!
+    
+    var arrays = [
+    ["Four + Two is equal to Six","True"],
+    ["Five - Three is greater than One","True"],
+    ["Three + Eight is less than Ten","False"],
+    ];
+    
+    var questionNumber = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        updateUI();
     }
-
-
+    
+    
+    @IBAction func answerBtnPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle;
+        let actualAnswer = arrays[questionNumber][1];
+        
+        if(userAnswer == actualAnswer){
+            print("Right");
+        }else{
+            print("Wrong");
+        }
+        
+        
+        if(questionNumber < arrays.count){
+            questionNumber+=1;
+            updateUI();
+        }else{
+            questionLabel.text = "No more question";
+        }
+    }
+    
+    func updateUI(){
+        questionLabel.text = arrays[questionNumber][0];
+    }
 }
 
